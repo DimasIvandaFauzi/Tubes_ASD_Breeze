93a7d9e465f745759ad720b30a2c277656eeed42
whorsyd <sukiman8gaming@gmail.com>
2024-05-09 13:20:59 +0700

    percobaan halaman_home

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
+Route::view('/home', 'home');
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
