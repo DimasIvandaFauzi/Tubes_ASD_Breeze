783126d826ccf4129cb5442097a8f6dfb699d529
whorsyd <sukiman8gaming@gmail.com>
2024-05-13 15:06:32 +0700

    error controller

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Menu;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -14,7 +14,8 @@ class MenuController extends Controller
     public function index()
     {
         $menu = Menus::all();
-        return view ('dashboard', ['menus' => $menu]);
+        return view ('menu', ['menus' => $menu]);
+        dd($menu);
     }
 
     /**
